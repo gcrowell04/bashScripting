55e9779b52b66c2886b2853032b96c852fc7b351
gcrowell04 <gcrowell04@gmail.com>
2025-04-22 14:06:48 -0500

    Update monitorFilesScript.sh
    Add functionality to automate new thread creation

--- monitorFilesScript.sh
+++ monitorFilesScript.sh
@@ -1,9 +1,19 @@
-while true;
-  do echo "Thread count:";
-  ps -ef | grep <scriptName> | grep -v bash | grep -v grep | wc -l; ## active threadsCount
-  ps -ef --sort=start_time | grep <scriptName> | grep -v bash | grep -v grep; # List scripts and sort by age
-  echo "File output:";
-  ps -ef --sort=start_time | grep <scriptName> | grep -v bash | grep -v grep;
-  tail -n 10 $(ls *js.out-$(date +%m-%d-%Y)); ## Dynamically follow the output of files based on today's date
-  sleep 25; ## wait for 25 seconds
-done
+        clear;
+        while true;
+                do echo "Thread count:";
+                 tc = ps -ef | grep engine | grep -v bash | grep -v grep | wc -l;
+
+                if [$tc < 6];  then
+                        file_to_run=$(find . -name "*.js" -quit);
+                        echo "Thread Finished. Starting new file";
+                        echo $file_to_run;
+                        nohup <script_name> $file_to_run &
+                        mv /home/$(LOGNAME)/$file_to_run /home/$(LOGNAME)/processed
+                else
+                    echo "6 active threads"
+                fi
+                 echo "File output:";
+                 ps -ef --sort=start_time | grep engine | grep -v bash | grep -v grep;
+                 tail -n 10 $(ls *js.out-prod-$(date +%m-%d-%Y));
+                 sleep 25;
+         done
